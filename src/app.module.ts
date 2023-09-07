0296bd6848442f1573407c6a117d448902799b73
Giovanne Mendonça <giovannemendonca@outlook.com>
2023-09-07 19:49:36 -0300

    feat: Initial Prisma configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { prismaService } from './prisma/prisma.service'
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, prismaService],
 })
 export class AppModule {}
